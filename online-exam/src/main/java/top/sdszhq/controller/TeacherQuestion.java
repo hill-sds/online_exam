463b0f7108fe2f9875b15d30bfe14cea4662b643
hill-sds <46988800+hill-sds@users.noreply.github.com>
2019-05-06 17:12:23 +0800

    test

--- online-exam/src/main/java/top/sdszhq/controller/TeacherQuestion.java
+++ online-exam/src/main/java/top/sdszhq/controller/TeacherQuestion.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import top.sdszhq.pojo.Question;
 import top.sdszhq.service.TeacherQuestionService;
+import top.sdszhq.utils.CookieUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,16 +22,30 @@ public class TeacherQuestion {
 
     @ResponseBody
     @RequestMapping(value = "/html/seekAll.action",produces = "application/json; charset=utf-8")
-    public String seekAll() {
+    public String seekAll(HttpServletRequest request) {
         System.out.println("666");
         List<Question> list = new ArrayList<>();
-        list = tqs.findQuestionAll();
+        String person = CookieUtil.getCookie(request,"name");
+        list = tqs.findQuestionAll(person);
         return new Gson().toJson(list);
     }
 
     @ResponseBody
     @RequestMapping(value = "/html/addQuestion.action",produces = "application/json; charset=utf-8")
-    public String addQuestion(@RequestBody Map<String,String> map) {
+    public String addQuestion(@RequestBody Map<String,String> map, HttpServletRequest request) {
+        String question = map.get("question");
+        String answer = map.get("answer");
+        String optionA = map.get("optionA");
+        String optionB = map.get("optionB");
+        String optionC = map.get("optionC");
+        String optionD = map.get("optionD");
+        String subject = map.get("subject");
+        String difficulty = map.get("difficulty");
+        String book = map.get("book");
+        String chapter = map.get("chapter");
+        String person = CookieUtil.getCookie(request,"name");
+        Question qt = new Question(question,answer,optionA,optionB,optionC,optionD,subject,person,book,chapter,difficulty);
+        tqs.addQuestion(qt);
         System.out.println("666");
         List<Question> list = new ArrayList<>();
         return new Gson().toJson(list);
