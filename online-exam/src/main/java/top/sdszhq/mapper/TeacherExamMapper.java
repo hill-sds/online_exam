42abf987644e1d74cfd63d45a3bdd3d615709417
hill-sds <46988800+hill-sds@users.noreply.github.com>
2019-05-08 16:12:21 +0800

    test

---
+++ online-exam/src/main/java/top/sdszhq/mapper/TeacherExamMapper.java
@@ -0,0 +1,16 @@
+package top.sdszhq.mapper;
+
+import top.sdszhq.pojo.Exam;
+import top.sdszhq.pojo.Question;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface TeacherExamMapper {
+
+    //添加试卷
+    void addExam(Exam ea);
+
+    //根据条件查询试卷
+    List<Exam> findExam(HashMap map);
+}
