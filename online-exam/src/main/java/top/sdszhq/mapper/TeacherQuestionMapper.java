8fec0fc665b1c1860d5706cf717181b28fb8b88c
hill-sds <46988800+hill-sds@users.noreply.github.com>
2019-05-08 16:36:39 +0800

    test

--- online-exam/src/main/java/top/sdszhq/mapper/TeacherQuestionMapper.java
+++ online-exam/src/main/java/top/sdszhq/mapper/TeacherQuestionMapper.java
@@ -2,9 +2,16 @@ package top.sdszhq.mapper;
 
 import top.sdszhq.pojo.Question;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface TeacherQuestionMapper {
-    //查询所有记录
-    List<Question> findQuestionAll();
+    //查询所有试题
+    List<Question> findQuestionAll(String name);
+
+    //根据条件查询试题
+    List<Question> findQuestion(HashMap map);
+
+    //添加试题
+    void addQuedtion(Question qt);
 }
