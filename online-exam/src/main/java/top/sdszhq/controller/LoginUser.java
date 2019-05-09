b1ebcfe6aaaedb2896e2cb32157151df30344dff
hill-sds <46988800+hill-sds@users.noreply.github.com>
2019-05-09 18:43:03 +0800

    test

--- online-exam/src/main/java/top/sdszhq/controller/LoginUser.java
+++ online-exam/src/main/java/top/sdszhq/controller/LoginUser.java
@@ -3,14 +3,19 @@ package top.sdszhq.controller;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import top.sdszhq.pojo.ExamInformation;
+import top.sdszhq.pojo.Question;
 import top.sdszhq.service.LoginService;
+import top.sdszhq.utils.CookieUtil;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 public class LoginUser {
@@ -20,59 +25,78 @@ public class LoginUser {
 
     //学生登录
     @ResponseBody
-    @RequestMapping("*/login.action")
-    public String selectId(String account, String password, HttpServletResponse response) {
-        Map pas = new HashMap();
-        pas = ls.selectPassword(account);
-        Map map = new HashMap();
-        if(pas==null) {
-            map.put("msg", "null");
-        }else {
-            if(pas.get("password").equals(password)) {
-                map.put("msg", "yes");
+    @RequestMapping(value = "*/login.action",produces = "application/json; charset=utf-8")
+    public String selectId(@RequestBody Map<String,String> map, HttpServletResponse response) {
+        String account = map.get("account");
+        String password = map.get("password");
+        String status = map.get("status");
+        Map rtnMap = new HashMap();
+
+        if(status.equals("student")) {
+            rtnMap = ls.selectPassword(account);
+            if(rtnMap==null) {
+                map.put("msg", "null");
             }else {
-                map.put("msg", "no");
+                if(rtnMap.get("password").equals(password)) {
+                    map.put("msg", "studentYes");
+                    Cookie nameCookie = new Cookie("name", (String) rtnMap.get("name"));
+                    response.addCookie(nameCookie);
+                }else {
+                    map.put("msg", "no");
+                }
+            }
+        }
+        if(status.equals("teacher")) {
+            rtnMap = ls.selectTeacherPassword(account);
+            if(rtnMap==null) {
+                map.put("msg", "null");
+            }else {
+                if(rtnMap.get("password").equals(password)) {
+                    map.put("msg", "teacherYes");
+                    Cookie nameCookie = new Cookie("name", (String) rtnMap.get("name"));
+                    response.addCookie(nameCookie);
+                }else {
+                    map.put("msg", "no");
+                }
             }
         }
         return new Gson().toJson(map);
     }
 
     @ResponseBody
-    @RequestMapping("login.action")
-    public String selectId2(String account, String password, HttpServletResponse response) {
-        Map pas = new HashMap();
-        pas = ls.selectPassword(account);
-        Map map = new HashMap();
-        if(pas==null) {
-            map.put("msg", "null");
-        }else {
-            if(pas.get("password").equals(password)) {
-                map.put("msg", "yes");
+    @RequestMapping(value = "login.action",produces = "application/json; charset=utf-8")
+    public String selectId2(@RequestBody Map<String,String> map, HttpServletResponse response) {
+        String account = map.get("account");
+        String password = map.get("password");
+        String status = map.get("status");
+        Map rtnMap = new HashMap();
+
+        if(status.equals("student")) {
+            rtnMap = ls.selectPassword(account);
+            if(rtnMap==null) {
+                map.put("msg", "null");
             }else {
-                map.put("msg", "no");
+                if(rtnMap.get("password").equals(password)) {
+                    map.put("msg", "studentYes");
+                    Cookie nameCookie = new Cookie("name", (String) rtnMap.get("name"));
+                    response.addCookie(nameCookie);
+                }else {
+                    map.put("msg", "no");
+                }
             }
         }
-        return new Gson().toJson(map);
-    }
-
-    //教师登录
-    @ResponseBody
-    @RequestMapping("/html/teacherLogin.action")
-    public String teacherLogin(String account, String password, HttpServletResponse response) {
-        Map pas = new HashMap();
-        pas = ls.selectTeacherPassword(account);
-        Map map = new HashMap();
-        if(pas==null) {
-            map.put("msg", "null");
-        }else {
-            if(pas.get("password").equals(password)) {
-                map.put("msg", "yes");
-                String name = (String) pas.get("name");
-                Cookie nameCookie = new Cookie("name", (String) pas.get("name"));
-                /*Cookie cookie1 = new Cookie("status",(String) pas.get("status"));*/
-                response.addCookie(nameCookie);
+        if(status.equals("teacher")) {
+            rtnMap = ls.selectTeacherPassword(account);
+            if(rtnMap==null) {
+                map.put("msg", "null");
             }else {
-                map.put("msg", "no");
+                if(rtnMap.get("password").equals(password)) {
+                    map.put("msg", "teacherYes");
+                    Cookie nameCookie = new Cookie("name", (String) rtnMap.get("name"));
+                    response.addCookie(nameCookie);
+                }else {
+                    map.put("msg", "no");
+                }
             }
         }
         return new Gson().toJson(map);
